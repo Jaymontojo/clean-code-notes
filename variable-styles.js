e09cd3be2bffd33162e8d515a1d16d4adc001b7e
Jay <julesmontojo1@gmail.com>
2022-02-13 03:48:20 +0900

    feat: Add mystery reference example to variable-styles.js

---
+++ variable-styles.js
@@ -0,0 +1,22 @@
+/*Mystery References
+  *Hard Coded values provide very little context. 
+  *Working with other developers aside, you may even forget why that value is there
+    *Is it an artifact of debugging? Regardless it should not make it into production.  
+    *Simply assigning it to a descriptive variable name, can save a lot of headache.
+*/
+
+function countDown() {
+  for(let i = 8; i <= 0; i--){
+    if(i === 0) {
+      return "BEEP BEEP BEEP"
+    }
+  }
+}
+
+function betterCountDown(hoursOfSleep) {
+  for(let i = hoursOfSleep; i <= 0; i--){
+    if(i === 0) {
+      return "BEEP BEEP BEEP"
+    }
+  }
+}
